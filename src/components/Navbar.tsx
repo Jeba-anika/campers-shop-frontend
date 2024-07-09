d812329d104ca587315b095e013b72c44ef86e19
Jeba Anika <anikanur98@gmail.com>
2024-07-09 13:04:38 +0600

    chore: router setup done

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import logo from "../assets/logo.png";
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+        <img src={logo} alt="campers-shop-logo"></img>
+        <h1>Campers Shop</h1>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ export default {
   content: ["./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
+    colors: {
+      'primary': '#466A8B',
+      'cs-ash': '#8B949D',
+      'neutral': '#FFFCF7',
+      'secondary': '#EDE2D7',
+      'tertiary': '#ABA08F',
+      'highlight': '#493725'
+    },
     extend: {},
   },
   plugins: [],
