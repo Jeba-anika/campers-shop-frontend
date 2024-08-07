d4b5074b8a8817cef7b7ebfcb51e8fa45f48d737
Jeba Anika <anikanur98@gmail.com>
2024-08-08 00:43:24 +0600

    chore: reload alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 
 function App() {
+  useEffect(() => {
+    const handleBeforeUnload = (event: any) => {
+      const message =
+        "You have items in your cart. Are you sure you want to leave?";
+      event.returnValue = message;
+      alert(message);
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, []);
   return (
     <>
       <Navbar />
